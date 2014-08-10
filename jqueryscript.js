185cb8c6e1b4384af0263f087f91610f13c90a6e
TSchertenleib <thomas.schertenleib@gmail.com>
2014-08-10 15:31:31 +0200

    jquery on hover

--- jqueryscript.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function() {
-	$('#navigation1').on('click', '#musik', function() {
-	$(this).closest('#navigation1').find('.unterliste').slideDown();
-	});
-});
---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function ()	{
+	
+	$('h2').css('font-size', '40px');
+	$('.slider').slick({
+	});
+
+    $('.unterliste').hover(function() {
+        $(this).addClass('transition');
+ 
+    }, function() {
+        $(this).removeClass('transition');
+    });
+
+
+
+/* 	$( ".video1" ).offset({left: -400 }); */
+/* 	$( ".video2" ).offset({left: 300 }); */
+	
+	
+	
+	
+});
+
